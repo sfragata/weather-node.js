991cb1dad90ad56482bad00a8f0af7965b25d0ec
Silvio Fragata <silviofragata@yahoo.com.br>
2014-09-10 23:10:44 -0400

    create multiple modules(cities and forecast)

---
+++ modules/cities.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var log4js = require('log4js');
+
+var cityListLogger = log4js.getLogger("/weather/city/list");
+
+var url = "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=";
+
+exports.list = function(req, res){
+	var bodyResp = '';
+	var data = req.body;
+	cityListLogger.info("request query: %s", data.city);
+	http.get(url + data.city, function(httpResponse) {
+		httpResponse.setEncoding('utf8');
+
+		if( httpResponse.statusCode != 200 ){
+			cityListLogger.error("status code %s", httpResponse.statusCode);
+			res.send(httpResponse.statusCode);
+			return;
+		}
+
+		httpResponse.on("data", function(chunk){
+			bodyResp += chunk;
+			//console.log("%s:got data...", req.path);
+		});
+
+		httpResponse.on('end', function () {
+			bodyResp = bodyResp.replace("?", "");
+			bodyResp = bodyResp.replace("(", "");
+			bodyResp = bodyResp.replace(");", "");
+			bodyResp = "{\"cities\":" + bodyResp + "}";
+			//console.log("httpResponse: %s", bodyResp);
+			var respJson = JSON.parse(bodyResp);
+
+			res.status(200).send(respJson);
+		});
+	}).on('error', function(e) {
+		cityListLogger.error("got error: ", e);
+		res.send(500, e.message);
+	});
+};
---
+++ modules/forecast.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var log4js = require('log4js');
+
+var forecastLogger = log4js.getLogger("/weather/forecast");
+
+var url = "http://api.openweathermap.org/data/2.5/weather?mode=json&units=metric&q=";
+
+exports.retrieve = function(req, res){
+	var data = req.body;
+	forecastLogger.info("request city: %s", data.city);
+	var bodyResp = '';
+	http.get(url + data.city, function(httpResponse) {
+
+		if( httpResponse.statusCode != 200 ){
+			forecastLogger.error("status code %s", httpResponse.statusCode);
+			res.send(httpResponse.statusCode);
+			return;
+		}
+
+		httpResponse.on("data", function(chunk){
+			bodyResp += chunk;
+		});
+
+		httpResponse.on('end', function () {
+			var weatherJson = JSON.parse(bodyResp);
+			//{ message: 'Error: Not found city', cod: '404' }
+			if( weatherJson.cod != 200 ){
+				//add city to the json's response error
+				weatherJson.city = data.city;
+				forecastLogger.error('response: %s', JSON.stringify(weatherJson));
+				res.status(weatherJson.cod).send(weatherJson.message);
+			} else {
+				forecastLogger.info('response: %s', bodyResp);
+				res.render('weather.ejs', { weather: weatherJson });
+			}
+		});
+	}).on('error', function(e) {
+		forecastLogger.error("got error: ", e);
+	});	
+};
