23b9455364047feaf3a474a0308c3c70a5029eb8
Silvio Fragata Silva <silviofragata@yahoo.com.br>
2015-02-10 23:41:01 -0500

    minor changes| add new values to the grid| format date directly on angular view | remove moment.js | disable submit button while form is invalid

--- public/javascript/forecast-controller.js
+++ public/javascript/forecast-controller.js
@@ -7,45 +7,9 @@
       data.city = this.city;
       $http.post('/weather/forecast', JSON.stringify(data)).
         success(function(data, status, headers, config) {
-          var date = moment.unix(data.dt);
-          data.dt = date.format("YYYY-MM-DD HH:mm:ss");
-
-          var time = moment.unix(data.sys.sunrise);
-          data.sys.sunrise = time.format("HH:mm:ss");
-          time = moment.unix(data.sys.sunset);
-          data.sys.sunset = time.format("HH:mm:ss")
           console.log(data);
-
-          cityForecast.name = data.name;
-          cityForecast.sys = {};
-          cityForecast.sys.country = data.sys.country;
-          cityForecast.sys.sunrise = data.sys.sunrise;
-          cityForecast.sys.sunset = data.sys.sunset;
-
-          cityForecast.main = {};
-          cityForecast.main.temp = data.main.temp;
-          cityForecast.main.pressure = data.main.pressure;
-          cityForecast.main.humidity = data.main.humidity;
-          cityForecast.main.temp_min = data.main.temp_min;
-          cityForecast.main.temp_max = data.main.temp_max;
-
-          cityForecast.weather = [];
-          cityForecast.weather[0] = {};
-          cityForecast.weather[0].main = data.weather[0].main;
-          cityForecast.weather[0].icon="http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";   
-          cityForecast.weather[0].description = data.weather[0].description;
-
-          cityForecast.dt = data.dt;
-          cityForecast.wind = {};
-          cityForecast.wind.speed = data.wind.speed;
-          cityForecast.clouds ={};
-          cityForecast.clouds.all = data.clouds.all;
-
-          cityForecast.error = "";
-
-          cityForecast.coord = {};
-          cityForecast.coord.lat = data.coord.lat;
-          cityForecast.coord.lon = data.coord.lon;
+          //copy all data to the cityForecast
+          angular.copy(data, cityForecast);
 
           //Get google map
           var myLatlng = new google.maps.LatLng(cityForecast.coord.lat, cityForecast.coord.lon);
