04e0c1bcb9e05b1cc7b126cada1c803b36ee7cdd
Silvio Fragata Silva <silviofragata@yahoo.com.br>
2015-03-06 00:37:45 -0500

    - add scriptjs to load all javascripts - remove google maps api key - update README - add a description when click on mark on map

---
+++ public/javascript/load.js
@@ -0,0 +1,15 @@
+$script('javascript/jquery-1.10.2.min.js');
+$script('javascript/jquery-ui-1.10.4.custom.min.js');
+$script('javascript/angular.min.js', 'angular');
+
+$script.ready('angular', function(){
+
+	$script(['javascript/weather-angular.js','javascript/weather-controller.js',
+		'javascript/forecast-controller.js','javascript/forecast-directive.js',
+		'javascript/city-autocomplete-directive.js'], 'weather-bundle');	
+
+	$script.ready('weather-bundle', function() {
+		angular.bootstrap(document, ['weatherApp']); 
+		$script.get('https://maps.googleapis.com/maps/api/js?v=3.exp&callback=$', function(){});
+	});
+});
